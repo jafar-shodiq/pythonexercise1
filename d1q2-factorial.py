2f1fc6b3b58947a8232ade980505cb19c067047e
Jafar Shodiq <j.shodiq2312@gmail.com>
2020-10-13 18:32:36 +0700

    first commit

---
+++ d1q2-factorial.py
@@ -0,0 +1,8 @@
+my_input = int(input('input: '))
+
+fact = 1
+
+for i in range(1, my_input+1):
+    fact = fact * i
+
+print(fact)
---
+++ d1q3-dictsquare.py
@@ -0,0 +1,5 @@
+my_input = int(input('input: '))
+
+ans = {i: i*i for i in range(1, my_input+1)}
+
+print(ans)

2e4fd56164089a2fb7bbacc8c29cef38e55f0915
Jafar Shodiq <j.shodiq2312@gmail.com>
2020-10-28 08:12:33 +0700

    first commit

---
+++ d9q28-strsum.py
@@ -0,0 +1,4 @@
+def add(num1, num2):
+    return int(num1) + int(num2)
+
+print(add('4', '5'))
---
+++ d9q29-strconc.py
@@ -0,0 +1,4 @@
+def conc(str1, str2):
+     return str1 + str2
+    
+print(conc('i like ', 'sushi.'))

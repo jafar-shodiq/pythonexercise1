1b0cad56cb0272bd1bd729510a77aa002844ac16
Jafar Shodiq <j.shodiq2312@gmail.com>
2020-10-30 14:27:54 +0700

    first commit

---
+++ d10q31-dict2.py
@@ -0,0 +1,9 @@
+def my_dict():
+    d = dict()
+    for i in range (1, 21):
+        d[i] = i**2
+    # for k in d.keys():
+    #     print(k)
+    print(d.keys())
+
+print(my_dict())
---
+++ d10q33-list.py
@@ -0,0 +1,7 @@
+def my_list():
+    li = list()
+    for i in range(1, 21):
+        li.append(i**2)
+    print(li)
+
+print(my_list())

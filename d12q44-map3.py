5dcdb046b373b3f22d1c674e8441caf676f4fb21
Jafar Shodiq <j.shodiq2312@gmail.com>
2020-11-10 12:55:34 +0700

    first commit

---
+++ d12q44-map3.py
@@ -0,0 +1,2 @@
+squared = list(map(lambda x:x**2, range(1,21)))
+print(squared)
---
+++ d12q45-staticmethod.py
@@ -0,0 +1,8 @@
+class American():
+    @staticmethod
+    def printNationality():
+        print('American')
+    
+American1 = American()
+American1.printNationality()
+American.printNationality()

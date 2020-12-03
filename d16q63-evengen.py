340f14b6c9b1c12c5aa9389d9e7394d9bd81445b
Jafar Shodiq <j.shodiq2312@gmail.com>
2020-12-03 13:10:21 +0700

    first commit

---
+++ d16q63-evengen.py
@@ -0,0 +1,13 @@
+def even_generator(n):
+    i = 0
+    while i <= n:
+        if i%2 == 0:
+            yield i
+        i+=1
+
+n = int(input('Input: '))
+values = []
+for i in even_generator(n):
+    values.append(str(i))
+
+print(','.join(values))
